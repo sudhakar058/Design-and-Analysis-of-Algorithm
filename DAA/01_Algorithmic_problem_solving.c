6e259415f34f4a2d022dc34168daf943893ecc15
sudhakar058 <sudhakar02911@gmail.com>
2025-10-31 15:06:48 +0530

    Add files via upload

---
+++ DAA/01_Algorithmic_problem_solving.c
@@ -0,0 +1,90 @@
+//Constant Complexity - O(1)
+#include <stdio.h> 
+int main() 
+{
+ int a = 4;
+ int b = 6;
+ int c;
+ c = a + b; 
+printf("%d", c);
+  }
+
+  // EX2-Linear time – O(n)
+  for (int i = 1; i <= n; i += c)
+  { 
+ // some expressions 
+ }
+ 
+ 
+ function(int n) { 
+     if (n==1) 
+        return; 
+     for (int i=1; i<=n; i++) 
+     { 
+         for (int j=1; j<=n; j++) 
+         { 
+             printf("*"); 
+             break; 
+         }     } 
+ 
+ 
+          int m=0;
+          for (int i = 0; i < n; i++) {
+              m=m+1;
+   }
+//Quadratic Complexity - O(n^2)
+#include <stdio.h>
+int main()
+{
+int i,j,n = 8;
+for (int i = 1; i <= n; i++)
+{
+for (int j = 1; j <= n; j++)
+{
+printf("VELTECH\n");
+} } } 
+
+
+for (int i = 1; i <=n; i += c) 
+{ 
+	for (int j = 1; j <=n; j += c)
+		 { 
+		   // some expressions 
+		} 
+}
+
+int m=0;
+       // Executed n times
+         for (int i = 0; i < n; i++) {
+             m=m+1;
+  }
+    // outer loop executed n times
+   for (int i = 0; i < n; i++) {
+// inner loop executed n times
+       for(int j = 0; j < n; j++)
+             m=m+1;
+  }
+
+  //Logarithmic time – O (log n)
+  for (int i = 1; i <=n; i *= c) 
+{ 
+		// some expressions 
+} 
+for (int i = n; i > 0; i /= c) 
+{ 
+		// some expressions
+ }
+//EX5- O(nlogn) — A log-linear algorithm
+int m=0; 
+
+//Cubic time – O (n3)
+int m=0;
+      
+    // outer loop executed n/2 times
+   for (int i = n/2; i < n; i++) {
+// middle loop executed n/2 times
+       for(int j = n/2; j < n; j++)
+      // inner loop executed n times
+          for(int k=0;k < n; k++ )
+             m=m+1;
+  }
